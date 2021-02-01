351340e0c80a683b618b758633eec1a7d43263d2
Zach Doucet <zachdouc@gmail.com>
2021-02-01 10:10:01 -0700

    idk

--- cc-events/dd-image-description-ex/js/main.js
+++ cc-events/dd-image-description-ex/js/main.js
@@ -1,1 +1,18 @@
 //Put your solution in this file
+const featureLink = document.querySelector('p.city-intro a');
+
+featureLink.addEventListener('click', featureLinkHandler);
+
+function linkHandler(evt) {
+    if( confirm('Ok to stay on this page, Cancel to leave and go to nait') ) {
+        //preventDefault stops the normal operation of the href.
+        evt.preventDefault();
+    }    
+}
+//Use the debugger in Chrome to see what is happening at each statement.
+function featureLinkHandler(evt) {     
+    let featureImage = document.querySelector('img.feature');     
+    featureImage.src = featureLink.href;     
+    featureImage.classList.remove('hidden');
+    evt.preventDefault();
+}
